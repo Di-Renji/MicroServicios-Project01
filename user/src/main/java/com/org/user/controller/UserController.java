1731e0e06df6b09dca0bcec010ffc2a0cc9a15ac
Joel DR <joel_la@outlook.es>
2023-03-15 06:52:23 -0500

    servicio User, finalizado

---
+++ user/src/main/java/com/org/user/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.org.user.controller;
+
+import com.org.user.entity.User;
+import com.org.user.service.UserService;
+import org.hibernate.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<User>> getAll(){
+        List<User> users = userService.listar();
+        if (users.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<User> getById(@PathVariable("id") int id){
+        User user = userService.buscar(id);
+        if (user == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<User> save (@RequestBody User user){
+        User userNew = userService.registrar(user);
+        return ResponseEntity.ok(userNew);
+    }
+}
---
+++ user/src/main/java/com/org/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.org.user.repository;
+
+import com.org.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
